8055429e2abca0c34809e9d441d03bd58716ad5b
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2022-07-14 21:28:48 +0800

    Add raw link generation script

---
+++ gen-raw-links.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# This script makes submitting builds to COPR via raw URL easier
+
+for SPEC in *.spec; do
+    echo "https://raw.githubusercontent.com/cyqsimon/el-rust-pkgs-spec/master/${SPEC}"
+done
