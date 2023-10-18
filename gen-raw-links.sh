2d428844163f25df0a9aa5f467618093e1514e41
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2023-10-19 01:35:37 +0800

    Rm old, unused script & add status check script

--- gen-raw-links.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# This script makes submitting builds to COPR via raw URL easier
-
-for SPEC in specs/*.spec; do
-    echo "https://raw.githubusercontent.com/cyqsimon/el-rust-pkgs-spec/master/${SPEC}"
-done
---
+++ get-build-status.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Query the COPR API for build statuses of all packages, then print anything
+# that hasn't succeeded (including in-progress, etc.)
+
+set -e
+
+# API documentation is WIP; see https://github.com/fedora-copr/copr/issues/2809
+LIST_QUERY='https://copr.fedorainfracloud.org/api_3/package/list?ownername=cyqsimon&projectname=el-rust-pkgs&with_latest_build=1'
+BUILD_QUERY_BASE='https://copr.fedorainfracloud.org/coprs/build'
+
+curl -LsSf "$LIST_QUERY" | jq -r --arg BUILD_QUERY_BASE "$BUILD_QUERY_BASE" \
+    '.items
+    | map({ name, last_build_id: .builds.latest.id, last_build_status: .builds.latest.state })
+    | map(select(.last_build_status != "succeeded"))
+    | sort_by(.name)
+    | map("Build for \"\(.name)\" \(.last_build_status).\n  URL: $BUILD_QUERY_BASE/\(.last_build_id)")
+    | select(length != 0) // ["All succeeded."]
+    | join("\n")'
