76e317fde911a3abb0c1a0dc7149c05cfce3d534
cyqsimon <28627918+cyqsimon@users.noreply.github.com>
2023-12-14 12:47:04 +0800

    Add `-a`/`--all` flag to status check script

--- get-build-status.sh
+++ get-build-status.sh
@@ -1,19 +1,55 @@
 #!/usr/bin/env bash
 
 # Query the COPR API for build statuses of all packages, then print anything
-# that hasn't succeeded (including in-progress, etc.)
+# that hasn't succeeded (including in-progress, etc.).
+# Alternatively, print the newest build of all packages with the `--all` flag.
 
 set -e
+
+# default
+MODE=failed
+
+# handle arguments
+for ARG in "$@"; do
+    case $ARG in
+        -a | --all)
+            MODE=all
+            ;;
+        *)
+            echo "Unknown argument: \"$ARG\""
+            exit 1
+            ;;
+    esac
+done
 
 # API documentation is WIP; see https://github.com/fedora-copr/copr/issues/2809
 LIST_QUERY='https://copr.fedorainfracloud.org/api_3/package/list?ownername=cyqsimon&projectname=el-rust-pkgs&with_latest_build=1'
 BUILD_QUERY_BASE='https://copr.fedorainfracloud.org/coprs/build'
 
-curl -LsSf "$LIST_QUERY" | jq -r --arg BUILD_QUERY_BASE "$BUILD_QUERY_BASE" \
+ALL_STATUS=$(curl -LsSf "$LIST_QUERY" | jq \
     '.items
-    | map({ name, last_build_id: .builds.latest.id, last_build_status: .builds.latest.state })
-    | map(select(.last_build_status != "succeeded"))
-    | sort_by(.name)
-    | map("Build for \"\(.name)\" \(.last_build_status).\n  URL: \($BUILD_QUERY_BASE)/\(.last_build_id)")
-    | select(length != 0) // ["All succeeded."]
-    | join("\n")'
+    | map({ name, build: .builds.latest })
+    | sort_by(.name)')
+
+case $MODE in
+    failed)
+        SELECTED_STATUS=$(jq 'map(select(.build.state != "succeeded"))' <<< "$ALL_STATUS")
+        ;;
+    all)
+        SELECTED_STATUS="$ALL_STATUS"
+        ;;
+esac
+
+SELECTED_FORMATTED=$(jq --arg BUILD_QUERY_BASE "$BUILD_QUERY_BASE" \
+    'map("\(.name)-\(.build.source_package.version): \(.build.state)
+    URL: \($BUILD_QUERY_BASE)/\(.build.id)")' \
+    <<< "$SELECTED_STATUS")
+
+case $MODE in
+    failed)
+        jq -r 'select(length != 0) // ["All succeeded."] | join("\n")' <<< "$SELECTED_FORMATTED"
+        ;;
+    all)
+        jq -r 'join("\n")' <<< "$SELECTED_FORMATTED"
+        ;;
+esac
